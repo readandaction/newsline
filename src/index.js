d88c212b79bdd69b456ff7aeb98f4353ca83dd55
Chanjong Baek <readandaction@gmail.com>
2020-05-07 12:14:51 +0900

    Add politics contents

---
+++ src/index.js
@@ -0,0 +1,26 @@
+<div class="news">
+  <h3 class="news__title">최문순 도지사의 꿈, 차이나타운</h3>
+  <div class="news__contents">
+    <ul>
+      <li>
+        <a href="https://www.nocutnews.co.kr/news/4026765" target="blank">
+          최문순 "동해안에 차이나타운 조성" 2014-05-19
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://news.heraldcorp.com/view.php?ud=20150123000604&md=20150123125739_BL"
+          target="blank"
+        >
+          ‘정동진 차이나드림시티’ 지역경제활성화완 ‘한몸’ 2015. 01. 23
+        </a>
+      </li>
+      <li>
+        <a href="https://www.fnnews.com/news/201912050950029225" target="blank">
+          최문순 강원도지사, ‘중국복합문화타운 조성사업’ 런칭식 참석 2019. 12.
+          05
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>;
